dc8dd162eae503a27fb205a94f58df49513685e9
alexlin93 <alexlinica@gmail.com>
2016-11-17 12:13:35 -0800

    added comments

--- media.py
+++ media.py
@@ -3,13 +3,14 @@ import webbrowser
 class Movie():
 	""" This class provides a way to store movie related information """
 
-	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+	# Initializes inputs into objects 
+	def __init__(self, movie_title, movie_storyline, 
+		poster_image, trailer_youtube):
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 
+	# show_trailer function will open a webbrowser with trailer url
 	def show_trailer(self):
 		webbrowser.open(self.trailer_youtube_url)
